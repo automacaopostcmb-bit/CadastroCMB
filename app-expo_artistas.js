072e34fdaf2cfc4a54c4426bcd21e05e6113e4d4
automacaopostcmb-bit <automacaopostcmb@gmail.com>
2025-09-04 18:21:25 -0300

    Update app-expo_artistas.js

--- app-expo_artistas.js
+++ app-expo_artistas.js
@@ -53,7 +53,7 @@ let fontsReady = false;
 // posição da plaquinha no canvas (1080x1350)
 let plaquinhaX = 70;  // + direita
 let plaquinhaY = 900;  // + baixo
-let plaquinhaScale = 0.8; // 1 = 100%, 0.85 = 85%, 1.25 = 125%
+let plaquinhaScale = 0.65; // 1 = 100%, 0.85 = 85%, 1.25 = 125%
 
 /* ===========================
    CANVAS
@@ -565,6 +565,7 @@ document.addEventListener('DOMContentLoaded', () => {
 window.enviarParaGoogle = enviarParaGoogle;
 window.baixarImagem = baixarImagem;
 window.goToMenu = goToMenu;
+
 
 
 
